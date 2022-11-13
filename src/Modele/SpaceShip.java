1764022b392d632d1ee669b0597e030809015b06
MinaBoudj <aminaboudjedir5@gmail.com>
2022-11-13 20:51:38 +0100

    programmation de la classe SpaceShip

--- src/Modele/SpaceShip.java
+++ src/Modele/SpaceShip.java
@@ -23,7 +23,7 @@ public class SpaceShip implements VisualObject{
 	private Position moveLeft() { 
 		Position pos;
 		if(super.orientation == 1) {
-			if(super.getX() == 0 ) {
+			if(super.getX() == 0 ) { //ne peut pas bouger il est au bord du plateau
 				return super.positon;
 			}else {
 				pos.x = super.getX() - 1;
@@ -33,10 +33,15 @@ public class SpaceShip implements VisualObject{
 		if(super.orientation == 2) {
 			if(super.getY() == 16 ) {
 				return super.positon;
-			}else {
-				pos.x = super.getX() - 1;
-				pos.y = super.getY() + 1;
 			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() + 1;
+            }else{
+                    pos.x = super.getX() + 1;
+				    pos.y = super.getY() + 1;
+            }
+            }
 		}
 		if(super.orientation == 3) {
 			if(super.getX() == 0 ) {
@@ -46,40 +51,202 @@ public class SpaceShip implements VisualObject{
 				pos.y = super.getY();
 			}
 		}if(super.orientation == 4) {
-			if(super.getX() == 0 ) {
+			if(super.getX() == 0 || (super.getX() == 13 && super.getY()%2 != 0) ) {
 				return super.positon;
 			}else {
-				pos.x = super.getX() - 1;
-				pos.y = super.getY();
+                if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+                }else{
+                    pos.x = super.getX()+ 1;
+				    pos.y = super.getY() - 1;
+                }
 			}
 		}if(super.orientation == 5) {
-			if(super.getX() == 0 ) {
+			if(super.getX() == 0 || (super.getX() == 0 && super.getY()%2 == 0) ) {
 				return super.positon;
 			}else {
-				pos.x = super.getX() - 1;
-				pos.y = super.getY();
-			}
+                if(super.getY()%2 == 0){
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY() - 1;
+                }else{
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+                }
+            }
 		}
 		if(super.orientation == 6) {
-			if(super.getX() == 0 ) {
+			if(super.getX() == 13 ) { //ne peut pas bouger
 				return super.positon;
 			}else {
-				pos.x = super.getX() - 1;
+                pos.x = super.getX() + 1;
 				pos.y = super.getY();
-			}
+            }
 		}
 		return pos;
 	}
 	
-	private Position moveRight() { }
-	private Position moveForward() { }
-	private Position turnAround() { }
+	private Position moveRight() {  //recuperer la position à droite de cette case
+        Position pos;
+		if(super.orientation == 1) {
+			if(super.getY() == 0 || (super.getX() == 13 && super.getY()%2 != 0)) { //ne peut pas bouger il est au bord du plateau
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+            }else{
+                    pos.x = super.getX() + 1 ;
+				    pos.y = super.getY() - 1;
+            }
+		}
+		if(super.orientation == 2) {
+			if(super.getX() == 0 ) { //premiere colone
+				return super.positon;
+			}else{
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY();
+            }
+        }
+		if(super.orientation == 3) {
+			if(super.getY() == 16 || (super.getX() == 13 && super.getY()%2 != 0) ) { //dernier ligne + deriner colone(pas tous)
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() + 1;
+            }else{
+                    pos.x = super.getX() + 1 ;
+				    pos.y = super.getY() + 1;
+            }
+		}
+        if(super.orientation == 4) {
+			if(super.getY() == 0 || (super.getX() == 0 && super.getY()%2 == 0) ) { //premier ligne + premier colone (pas toute)
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY() - 1;
+            }else{
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+            }
+		}
+		if(super.orientation == 5) {
+			if(super.getY() == 13) { //ne ppeut pas bouger dernier colonne
+				return super.positon;
+			}else {
+                pos.x = super.getX() + 1;
+				pos.y = super.getY();
+            }
+		}
+		if(super.orientation == 6) {
+			if(super.getY() == 16 ) { //ne peut pas bouger derniere ligne
+				return super.positon;
+			}else {
+                if(super.getY()%2 == 0){
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY() + 1;
+                }else{
+                    pos.x = super.getX();
+				    pos.y = super.getY() + 1;
+                }
+            }
+		}
+		return pos;
+    }
+
+	private Position moveForward() { //recuperer la position tous droit 
+        Position pos;
+		if(super.orientation == 1) {
+			if(super.getX() == 0 || super.getY() == 0) { //ne peut pas bouger il est au bord du plateau
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY() - 1;
+            }else{
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+            }
+		}
+		if(super.orientation == 2) {
+			if(super.getY() == 16 ||(super.getX() == 0 && super.getY()%2 == 0)) { //derniere ligne
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX() - 1;
+				    pos.y = super.getY() + 1;
+            }else{
+                    pos.x = super.getX();
+				    pos.y = super.getY() + 1;
+            }
+        }	
+		if(super.orientation == 3) {
+			if(super.getX() == 13 ) { //deriere colone
+				return super.positon;
+			}else {
+				pos.x = super.getX() + 1;
+				pos.y = super.getY();
+			}
+		}
+        if(super.orientation == 4) {
+			if(super.getX() == 0 ) { // premiere colonne
+				return super.positon;
+			}else {
+                pos.x = super.getX() - 1;
+				pos.y = super.getY();
+			}
+		}
+        if(super.orientation == 5) {
+			if(super.getY() == 0 || (super.getX() == 13 && super.getY()%2 != 0) ) {
+				return super.positon;
+			}else {
+                if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() - 1;
+                }else{
+                    pos.x = super.getX() + 1;
+				    pos.y = super.getY() - 1;
+                }
+            }
+		}
+		if(super.orientation == 6) {
+			if(super.getY() == 16 || (super.getX() == 13 && super.getY()%2 != 0)) { //ne peut pas bouger
+				return super.positon;
+			}
+            if(super.getY()%2 == 0){
+                    pos.x = super.getX();
+				    pos.y = super.getY() + 1;
+                }else{
+                    pos.x = super.getX() + 1;
+				    pos.y = super.getY() + 1;
+                }
+		}
+		return pos;
+    }
+
+	private void turnAround() { 
+        if( super.getOrientation() == 1)
+            super.setOrientation(6);
+        if( super.getOrientation() == 2)
+            super.setOrientation(5);
+        if( super.getOrientation() == 3)
+            super.setOrientation(4);
+        if( super.getOrientation() == 4)
+            super.setOrientation(3);
+        if( super.getOrientation() == 5)
+            super.setOrientation(2);
+        if( super.getOrientation() == 6)
+            super.setOrientation(1);
+    }
 	
 	public void move(ArrayList<ArrayList<Cell>> gameBoard, ArrayList<Movement> movement) {
-		for(int i=0; i<movement.size(); i++) {
+		for(int i=0; i<movement.size(); i++) { //parcourir tous les mouvements 
 			Position pos;
 			if(movement.get(i) == Movement.Left) {
 					pos = moveLeft();
+                    //verifier que la case peut contenir des SpaceShips si oui l'ajouter dans la liste sinon le spaceShip se deplace pas
 					gameBoard[pos.getX()][pos.getY()].spaceShips.add(this.SpaceShip);
 				}
 			if(movement.get(i) == Movement.Right) {
