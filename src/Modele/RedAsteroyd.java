b685cd560c969fbf4bb2c5fa4b9d25651a536997
MinaBoudj <aminaboudjedir5@gmail.com>
2022-11-19 19:21:42 +0100

    move de whiteBlueAsteroyd + WhiteRedAsteroyd

--- src/Modele/RedAsteroyd.java
+++ src/Modele/RedAsteroyd.java
@@ -19,28 +19,7 @@ public class RedAsteroyd extends  Asteroyd {
 		Position pos1, pos2;
 		//changer l'orientation selon la direction
 		//ne peu pas faire bouger un objet devant lui
-		if((super.orientation==1 && directions[0]==1) || (super.orientation==2 && directions[0]==3) ||(super.orientation==6 && directions[0]==4) ||
-		(super.orientation==5 && directions[0]==2) || (super.orientation==3 && directions[0]==6) ||(super.orientation==4 && directions[0]==5) ){
-			try{
-				pos1 = super.getPosition().getForward(super.orientation);
-				if(gameBoard[pos1.getX()][pos1.getY()]!= null && gameBoard[pos1.getX()][pos1.getY()] instanceof EmptyCell){ //case n'est pas null et vide
-					//il bouge selon la direction 1 de 2 case
-					pos2 = pos1.getForward(super.orientation);
-					if(gameBoard[pos2.getX()][pos2.getY()]!= null && gameBoard[pos2.getX()][pos2.getY()] instanceof EmptyCell){
-						//rien dans la case (il a bougé de 2 cases)
-						//super.setX(pos2.getX());
-						//super.setY(pos2.getY());
-					}else{//la 2eme case est sois null ou n'est pas vide
-						//le metre donc dans la 1eme
-						//super.setX(pos1.getX());
-						//super.setY(pos1.getY());
-					}
-				}//premiere case sois n'est pas vide sois null donc ne bouge pas 	
-			}catch(Exception e){//renvoie une position incorrect
-				//faire quoi quand c'est un asteroyd ??
-			}
-		}else{
-			try{
+		try{
 				//changer l'orientation
 				int neworientation;
 				if(super.getOrientation()==1 && directions[0]==2)
@@ -121,9 +100,7 @@ public class RedAsteroyd extends  Asteroyd {
 				}//premiere case sois n'est pas vide sois null donc ne bouge pas 	
 			}catch(Exception e){//renvoie une position incorrect
 				//faire quoi quand c'est un asteroyd ??
-			}
-		}		
-	}	//verifier que la case est pas null
-		
+			}		
+	}	
 }
 
