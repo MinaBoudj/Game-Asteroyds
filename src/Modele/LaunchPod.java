97d0265ac6be15bf1289ade6697f0a493cda056f
Maylis BOUILLON <maylis.bouillon@etu.univ-nantes.fr>
2022-11-19 16:46:31 +0000

    Replace LaunchPod.java

--- src/Modele/LaunchPod.java
+++ src/Modele/LaunchPod.java
@@ -8,7 +8,7 @@ public class LaunchPod extends Cell {
 
     // Constructeurs
     public LaunchPod(String imagePath, int orientation, int x, int y){
-        super(imagePath, orientation, pos, true);               // a faire mettre direct le nom des images concernés
+        super(imagePath, orientation, pos, true);              
     }
 
     public LaunchPod(String imagePath, int orientation, Position position){
