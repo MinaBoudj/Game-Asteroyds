be83d9bd5066e98c165abaea5418fd0a80cc3499
MinaBoudj <aminaboudjedir5@gmail.com>
2022-11-26 11:00:39 +0100

    test de SpaceShip + RedPortal +whitePortal

---
+++ src/Modele/test/SpaceShipTest.java
@@ -0,0 +1,18 @@
+package Modele.test;
+
+/**
+ * @author Amina
+ */
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertTrue;
+
+import Modele.SpaceShipTest;
+import Modele.Position;
+import Modele.Cell;
+
+
+public class SpaceShipTest {
+}
