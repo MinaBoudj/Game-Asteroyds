0fc4c34aed359dc50696ca396ffe116c010e30a5
MinaBoudj <aminaboudjedir5@gmail.com>
2022-11-27 20:39:22 +0100

    ajout de test player

---
+++ src/Modele/test/PlayerTest.java
@@ -0,0 +1,67 @@
+package Modele.test;
+
+/**
+ * @author Amina
+ */
+
+import org.junit.Test;
+import org.junit.Before;
+
+import Modele.Player;
+import Modele.SpaceShip;
+import Modele.Color;
+import Modele.Movement;
+
+public class PlayerTest {
+    private Player player;
+
+    @Before
+    public void constructPlayer() throws Exception {
+        player = new Player("amina",Color.Red,"rouge",3, new position(1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithNullOrientation() throws Exception {
+        new Player("amina",Color.Red,"rouge",0, new position(1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithNegativeOrientation() throws Exception {
+        new Player("amina",Color.Red,"rouge",-1, new position(1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithTooBigOrientation() throws Exception {
+        new Player("amina",Color.Red,"rouge",18, new position(1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithNegativeX() throws Exception {
+        new Player("amina",Color.Red,"rouge",3, new position(-1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithNegativeY() throws Exception {
+        new Player("amina",Color.Red,"rouge",3, new position(1,-3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testConstructorWithNullPosition() throws Exception {
+        new Player("amina",Color.Red,"rouge",3, null);
+    }
+
+    @Test
+    public void testConstructorWithGoodArguments() throws Exception {
+        new Player("amina",Color.Red,"rouge",3, new position(1,3));
+    }
+
+    @Test(expected = Exception.class)
+    public void testSetMovementsIndiceTabNegative() throws Exception {
+        player.setMovement(-1,Movement.Left);
+    }
+
+    @Test(expected = Exception.class)
+    public void testSetMovementsIndiceTabBig() throws Exception {
+        player.setMovement(7,Movement.Left);
+    }
+}
