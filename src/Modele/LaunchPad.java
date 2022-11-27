476a8619cebae9a28114f895612d0260b5081102
MinaBoudj <aminaboudjedir5@gmail.com>
2022-11-27 17:13:05 +0100

    corection des erreurs dans lunchPad

--- src/Modele/LaunchPad.java
+++ src/Modele/LaunchPad.java
@@ -7,11 +7,11 @@ package Modele;
 public class LaunchPad extends Cell {
 
     // Constructeurs
-    public LaunchPod(String imagePath, int orientation, int x, int y){
+    public LaunchPad(String imagePath, int orientation, int x, int y){
         super(imagePath, orientation, x, y , true);              
     }
 
-    public LaunchPod(String imagePath, int orientation, Position pos){
+    public LaunchPad(String imagePath, int orientation, Position pos){
         super(imagePath, orientation, pos, true);
     }
 }  
