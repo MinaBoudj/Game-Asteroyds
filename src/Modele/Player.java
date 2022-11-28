263b9aa0cd760fb05426ba5521f7e6274e39fe75
Juba <abbacijuba@gmail.com>
2022-11-28 16:35:44 +0100

    Finalisation des méthodes move pour Asteroïde bleu...

--- src/Modele/Player.java
+++ src/Modele/Player.java
@@ -14,7 +14,7 @@ public class Player {
 
 
     /* Constructeur */
-    Player(String name, Color color, String image, int orientation, Position pos) throws Exception{
+    public Player(String name, Color color, String image, int orientation, Position pos) throws Exception{
         this.name = name;
         this.relics = new ArrayList<Integer>();
         this.spaceShip = new SpaceShip(image, orientation, pos, color);
