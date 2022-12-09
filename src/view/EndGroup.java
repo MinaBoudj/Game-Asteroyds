ea76fbc40364ee71de70ca18e79e32d19be9254c
Maylis BOUILLON <maylis.bouillon@etu.univ-nantes.fr>
2022-12-09 08:36:23 +0000

    Affichage des relics de endGroup

--- src/view/EndGroup.java
+++ src/view/EndGroup.java
@@ -22,12 +22,7 @@ public class EndGroup extends Group {
     }
 
     public void updateEndScene(Executable exit, Executable mainMenu, String players){ 
-        try {
-            getChildren().add(ShapeConstructor.newImage("background", screenWidth*1.5,screenHeight*1.5, screenWidth/2,screenHeight/2, 1));
-        } catch (Exception e) {
-            getChildren().add(ShapeConstructor.newRectangle(Color.BLACK, screenWidth*1.5, screenHeight*1.5, screenWidth/2, screenHeight/2));
-        }
-        
+	
         ranking_player(players);
         Text resultsText = ShapeConstructor.newText("Results : ", Color.WHITE, screenWidth*0.28,screenHeight*0.1, screenWidth*0.45,screenHeight*0.125);
 		Rectangle paneResultats = ShapeConstructor.newRectangle(Color.web("A9A9A9",0.6), screenWidth, screenHeight*0.15, screenWidth, screenHeight*0.15);
@@ -74,12 +69,17 @@ public class EndGroup extends Group {
         //Collections.sort(player_over , Collections.reverseOrder());
         
     public void displayRanking(int nombre_players){
-        String [] places = {"First : ", "Second : ", "Third : ", "Fourth : ", "Fifth : ", "Sixth : "};
+        String [] places = {"First : ", "Second : ", "Third : ", "Fourth : ", "Fifth : ", "Sixth : "},
+                  playerRelics;
         switch(nombre_players){
             case 2:  
                 for (int i = 0; i < ranking_player.size() ; i++){
                     getChildren().addAll(ShapeConstructor.newText(places[i] + ranking_player.get(i)[1] , Color.WHITE, screenWidth*0.28,screenHeight*0.15, screenWidth*0.4,screenHeight*(0.25 + 0.18*i )),
                         ShapeConstructor.newText("Relics : " + ranking_player.get(i)[0] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.33,screenHeight*(0.30 +0.18*i)),
+                        playerRelics = ranking_player.get(i)[0].split("/");
+                        for (int j = 0 ; j < playerRelics.length ; j ++ ) {
+                            getChildren().add(ShapeConstructor.newImage("relic" + playerRelics[j], screenWidth*0.05, screenHeight*0.05, screenWidth*(0.33+0.15*(j+1)),screenHeight*(0.30 +0.18*i)));
+                        }
                         ShapeConstructor.newText("Condition of the Ship : " + ranking_player.get(i)[2] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.33,screenHeight*(0.35 +0.18*i)));
                 } break;
             case 3:
@@ -90,22 +90,38 @@ public class EndGroup extends Group {
 
             case 5:
                 getChildren().addAll(ShapeConstructor.newText(places[0] + ranking_player.get(0)[1] , Color.WHITE, screenWidth*0.28,screenHeight*0.15, screenWidth*0.4,screenHeight*(0.25)),
-                        ShapeConstructor.newText("Relics : " + ranking_player.get(0)[0] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.33,screenHeight*(0.30)),
+                        ShapeConstructor.newText("Relics : " + ranking_player.get(0)[0] , Color.WHITE, screenWidth*0.2,screenHeight*0.03, screenWidth*0.33,screenHeight*0.30),
+                        playerRelics = ranking_player.get(0)[0].split("/");
+                        for (int j = 0 ; j < playerRelics.length ; j ++ ) {
+                            getChildren().add(ShapeConstructor.newImage("relic" + playerRelics[j], screenWidth*0.05, screenHeight*0.05, screenWidth*(0.33+0.15*(j+1)),screenHeight*0.3));
+                        }
                         ShapeConstructor.newText("Condition of the Ship : " + ranking_player.get(0)[2] , Color.WHITE, screenWidth*0.28,screenHeight*0.10, screenWidth*0.33,screenHeight*(0.35)));
                 for (int i = 1; i < 3 ; i++){
                     getChildren().addAll(ShapeConstructor.newText(places[i] + ranking_player.get(i)[1] , Color.WHITE, screenWidth*0.28,screenHeight*0.05, screenWidth*0.2,screenHeight*(0.25+ 0.18*i )),
                         ShapeConstructor.newText("Relics : " + ranking_player.get(i)[0] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.2,screenHeight*(0.30 +0.18*i)),
+                        playerRelics = ranking_player.get(i)[0].split("/");
+                        for (int j = 0 ; j < playerRelics.length ; j ++ ) {
+                            getChildren().add(ShapeConstructor.newImage("relic" + playerRelics[j], screenWidth*0.05, screenHeight*0.05, screenWidth*(0.2+0.15*(j+1)),screenHeight*0.3));
+                        }
                         ShapeConstructor.newText("Condition of the Ship : " + ranking_player.get(i)[2] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.2,screenHeight*(0.35 +0.18*i)));
                 }
                 for (int i = 1; i < 3 ; i++){
                     getChildren().addAll(ShapeConstructor.newText(places[i] + ranking_player.get(i)[1] , Color.WHITE, screenWidth*0.28,screenHeight*0.05, screenWidth*0.65,screenHeight*(0.25+ 0.18*i )),
                         ShapeConstructor.newText("Relics : " + ranking_player.get(i)[0] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.65,screenHeight*(0.30 +0.18*i)),
+                        playerRelics = ranking_player.get(i)[0].split("/");
+                        for (int j = 0 ; j < playerRelics.length ; j ++ ) {
+                            getChildren().add(ShapeConstructor.newImage("relic" + playerRelics[j], screenWidth*0.05, screenHeight*0.05, screenWidth*(0.65+0.15*(j+1)),screenHeight*0.3));
+                        }
                         ShapeConstructor.newText("Condition of the Ship : " + ranking_player.get(i)[2] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.65,screenHeight*(0.35 +0.18*i)));
                 } break;
             case 6:
                 displayRanking(5);
                 getChildren().addAll(ShapeConstructor.newText(places[5] + ranking_player.get(5)[1] , Color.WHITE, screenWidth*0.28,screenHeight*0.05, screenWidth*0.33,screenHeight*0.8),
                             ShapeConstructor.newText("Relics : " + ranking_player.get(5)[0] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.33,screenHeight*0.85),
+                            playerrelics = ranking_player.get(i)[0].split("/");
+                            for (int j = 0 ; j < playerRelics.length ; j ++ ) {
+                                getChildren().add(ShapeConstructor.newImage("relic" + playerRelics[j], screenWidth*0.05, screenHeight*0.05, screenWidth*(0.33+0.15*(j+1)),screenHeight*0.85));
+                            }
                             ShapeConstructor.newText("Condition of the Ship : " + ranking_player.get(5)[2] , Color.WHITE, screenWidth*0.28,screenHeight*0.03, screenWidth*0.33,screenHeight*0.9)
                 );
                 break;
