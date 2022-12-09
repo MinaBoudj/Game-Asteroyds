b5e6fad599217351a7314a9600f6cdab9cfc2349
MinaBoudj <aminaboudjedir5@gmail.com>
2022-12-09 11:39:31 +0100

    correction  de move Blue

--- src/Modele/BlueAsteroyd.java
+++ src/Modele/BlueAsteroyd.java
@@ -25,39 +25,72 @@ public class BlueAsteroyd extends Asteroyd{
           int neworientation = calculeOrientation(directions[2]);
           pos1 = super.getPosition().getForward(neworientation); // Case en face.
           pos2 = pos1.getForward(neworientation); // Case derrière la case en face.
-
-          if(gameBoard[pos1.getY()][pos1.getX()]!= null && gameBoard[pos1.getY()][pos1.getX()] instanceof EmptyCell) { // Si la première case est valide
-              if (gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().size() == 0) { // Si la première case est vide
+          
+          if(gameBoard[pos1.getY()][pos1.getX()]!= null){
+            if(gameBoard[pos1.getY()][pos1.getX()] instanceof EmptyCell){//c une case vide
+              if (gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().size() == 0) { // la case ne contient aucun vaisseau
                 // Déplacement de l'astéroïde
                 EmptyCell nec = new EmptyCell(super.getPosition());
                 gameBoard[super.getPosition().getY()][super.getPosition().getX()] = nec;  
                 super.getPosition().setX(pos1.getX());
                 super.getPosition().setY(pos1.getY());
                 gameBoard[super.getPosition().getY()][super.getPosition().getX()] = this;
-
-              } 
-              else if (gameBoard[pos2.getY()][pos2.getX()]!= null && gameBoard[pos2.getY()][pos2.getX()] instanceof EmptyCell) { // Si la 2ème case est vide
-                    if (gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().size() == 0) {
+                //verification de la deusieme case 
+                if(gameBoard[pos2.getY()][pos2.getX()]!= null){
+                  if(gameBoard[pos2.getY()][pos2.getX()] instanceof EmptyCell){//c une case vide
+                    if (gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().size() == 0) { // la case ne contient aucun vaisseau
+                      // Déplacement de l'astéroïde de pos1 à pos2 et this de sa pos à pos1
+                      gameBoard[pos1.getY()][pos1.getX()].setPosition(pos2);
+                      gameBoard[pos2.getX()][pos2.getY()] = gameBoard[pos1.getY()][pos1.getX()];
                       
-                    // Déplacement de l'obstacle sur la case d'après A VERIFIER ICI :
-                    EmptyCell nec = new EmptyCell(super.getPosition()); // super au lieu de pos 1 ?
-                    gameBoard[super.getPosition().getY()][super.getPosition().getX()] = nec;
-                    super.getPosition().setX(pos2.getX()); // super au lieu de pos 1 ?
-                    super.getPosition().setY(pos2.getY()); // super au lieu de pos 1 ?
-                    gameBoard[super.getPosition().getY()][super.getPosition().getX()] = this;
-                    
-                    // Déplacement de l'astéroïde voulu A FAIRE ICI :
+                      super.getPosition().setX(pos1.getX());
+                      super.getPosition().setY(pos1.getY());
 
-                    } else {
-                    // Infliger des dégâts
-                    for(int i=0; i<gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().size(); i++){
-                      gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().get(i).minusStructurePoint(1);
+                      EmptyCell nec1 = new EmptyCell(super.getPosition());
+                      gameBoard[super.getPosition().getY()][super.getPosition().getX()] = nec1;  
+                      gameBoard[super.getPosition().getY()][super.getPosition().getX()] = this;
+
+                    }else{
+                      // Infliger des dégâts
+                      for(int i=0; i<gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().size(); i++){
+                        gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().get(i).minusStructurePoint(1);
+                      }
                     }
-                  }
+                  }//ne rien deplacer si c pas une case vide
+                }
+              }else{
+                // Infliger des dégâts
+                for(int i=0; i<gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().size(); i++){
+                  gameBoard[pos1.getY()][pos1.getX()].getLSpaceShips().get(i).minusStructurePoint(1);
+                }
               }
-           }
+            }else if(gameBoard[pos1.getY()][pos1.getX()] instanceof Asteroyd){//dans la premiere case c un asteroid
+                //verification de la deusieme case 
+                if(gameBoard[pos2.getY()][pos2.getX()]!= null){
+                  if(gameBoard[pos2.getY()][pos2.getX()] instanceof EmptyCell){//c une case vide
+                    if (gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().size() == 0) { // la case ne contient aucun vaisseau
+                      // Déplacement de l'astéroïde de pos1 à pos2 et this de sa pos à pos1
+                      gameBoard[pos1.getY()][pos1.getX()].setPosition(pos2);
+                      gameBoard[pos2.getY()][pos2.getX()] = gameBoard[pos1.getY()][pos1.getX()];
+                      
+                      super.getPosition().setX(pos1.getX());
+                      super.getPosition().setY(pos1.getY());
+
+                      EmptyCell nec = new EmptyCell(super.getPosition());
+                      gameBoard[super.getPosition().getY()][super.getPosition().getX()] = nec;  
+                      gameBoard[super.getPosition().getY()][super.getPosition().getX()] = this;
+
+                    }else{
+                      // Infliger des dégâts
+                      for(int i=0; i<gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().size(); i++){
+                        gameBoard[pos2.getY()][pos2.getX()].getLSpaceShips().get(i).minusStructurePoint(1);
+                      }
+                    }
+                  }//ne rien deplacer si c pas une case vide
+                }
+            }
           }
-        catch(Exception e) {
+        }catch(Exception e) {
           // Traitement de l'erreur (orientation négative)
             }
       }	
